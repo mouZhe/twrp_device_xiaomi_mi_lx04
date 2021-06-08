72d9f95b38cb04d5cc4c3b6947de42ce63e150d0
某哲 <50080113+mouZhe@users.noreply.github.com>
2021-06-08 20:19:59 +0800

    Update lx04.mk

--- lx04.mk
+++ lx04.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, device/xiaomi/lx04/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mi_lx04
-PRODUCT_NAME := mi_lx04
+PRODUCT_DEVICE := lx04
+PRODUCT_NAME := lx04
 PRODUCT_BRAND := XiaoAiTongXue
 PRODUCT_MANUFACTURER := XiaoMi
 PRODUCT_MODEL := LX04
