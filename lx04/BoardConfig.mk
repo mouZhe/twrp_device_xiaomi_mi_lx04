a28ccb1f931fdcae52ebf1671d73fe11cae6afdd
Hikaru <37921907+voipsy@users.noreply.github.com>
2021-06-10 08:26:52 +0800

    Update BoardConfig.mk
    ALLOW_MISSING_DEPENDENCIES := true

--- lx04/BoardConfig.mk
+++ lx04/BoardConfig.mk
@@ -112,4 +112,4 @@ TWRP_INCLUDE_LOGCAT := true
 # Flags
 # TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 # TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-ALLOW_MISSING_DEPENDENCIES := TRUE
+ALLOW_MISSING_DEPENDENCIES := true
